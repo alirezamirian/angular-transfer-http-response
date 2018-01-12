d31d03b249ea4adbc523ada680b60d8f8016dc2e
Alireza Mirian <alireza.mirian@gmail.com>
2018-01-13 01:18:10 +0330

    fix: correctly export public api

--- src/transfer-http-response.module.ts
+++ src/transfer-http-response.module.ts
@@ -1,8 +1,10 @@
 import {NgModule} from '@angular/core';
 import {TransferHttpResponseInterceptor} from './transfer-http-response-interceptor.service';
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
-import {RequestKeyExtractorService} from './request-key-extractor.service';
+import {REQUEST_CACHE_BASE_URLS, RequestKeyExtractorService} from './request-key-extractor.service';
 import {BrowserTransferStateModule} from '@angular/platform-browser';
+
+export {REQUEST_CACHE_BASE_URLS, RequestKeyExtractorService};
 
 @NgModule({
   imports: [BrowserTransferStateModule],
